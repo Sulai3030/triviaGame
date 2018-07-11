8cca70f45a99433eee81ab441d7f38f78926461b
sully34 <sssulai@gmail.com>
2018-07-11 17:13:40 -0400

    added functions to questions

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+var game;
+var score;
+var timer;
+var questions;
+var choices;
+var results;
+var win;
+var lost
+var unanswered;
+var counter;
+
+var trivia:
+{
+    var game = function(){
+        question: {
+        question1:
+        question2:
+        question3:
+    },
+    choices {
+        [c1,c2,c3]
+    },
+    answer{c1}
+};
+
+var game = 
+{
+    var: 'win'
+
+}
+
