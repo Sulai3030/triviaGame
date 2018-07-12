f9a66a0faffd4679848537b2d8419e1133bc7965
sully34 <sssulai@gmail.com>
2018-07-12 15:46:25 -0400

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+var trivia;
 var game;
 var score;
 var timer;
@@ -9,23 +10,28 @@ var lost
 var unanswered;
 var counter;
 
-var trivia:
+var trivia =
+[{
+    var questions = 
+        [Question: "Which movie dircted by Martin Scorcese and starring Joe Pesci, Robert Deniro, and Ray Liotta won the BAFTA Awar for Best Picture?",
+        choices:["My Cousin Vinny?", "My Big Fat Greek Wedding?", "Goodfellas?", "Air America?"],
+        correctAnswer:"Good Fellas"
+},
 {
-    var game = function(){
-        question: {
-        question1:
-        question2:
-        question3:
-    },
-    choices {
-        [c1,c2,c3]
-    },
-    answer{c1}
-};
+        Question:"Who had the first number one Rap Single?",
+        choices:["MC Hammer", "DJ Jazzy Jeff and the Fresh Prince", "Vanilla Ice?", "Public Enemy?"]
+        
+        Question:"Who lit the Olympics Torch at the 1996 Summer Olympics?",
+        choices:["The Counting Crows?", "Mayor Andrew Young?", "President Clinton", "Muhammed Ali","Chris Harwicke and Jenny McCarthy"]
+
+        Question3:["Which stunning pair of FBI Agents should my girlfriend been dating?",
+        choices:["Agent Scully?","Agent Starling?","Agent Cooper?", "Agent Mulder"]
 
 var game = 
 {
     var: 'win'
+    var: 'lost'
+    var 'unanswered'
 
-}
+}]];
 
