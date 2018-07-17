f323caddc2e24869383d9cb8378f522030bfd3e2
sully34 <sssulai@gmail.com>
2018-07-17 15:29:29 -0400

    updated javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,112 @@
-//var trivia;
-//var game;
+var trivia;
 //var score;
 //var timer;
 //var questions;
 //var choices;
 //var results;
-//var win;
-//var lost
-//var unanswered;
-//var counter;
+var win;
+var lost
+var unanswered;
+var timer;
+var updateDisplay;
+var gamearea;
+var game;
+var timeLeft;
 
-var trivia = questions;
-[{[
-     trivia = [
-        questions] ["Which movie dircted by Martin Scorcese and starring Joe Pesci, Robert Deniro, and Ray Liotta won the BAFTA Awar for Best Picture?"]
-        [choices]["My Cousin Vinny?", "My Big Fat Greek Wedding?", "Goodfellas?", "Air America?"]
-        [correctAnswer]["Good Fellas"]
-     ]:
-{
-    trivia = [
-        Question]
-        ["Who had the first number one Rap Single?"]
-        [choices]["MC Hammer", "DJ Jazzy Jeff and the Fresh Prince", "Vanilla Ice?", "Public Enemy?"]
-        [correctAnswer]["Vanilla Ice"]
-    ,
+let questions = [
+    {
+        question:
+        "Which movie dircted by Martin Scorcese and starring Joe Pesci, Robert Deniro, and Ray Liotta won the BAFTA Awar for Best Picture?",
+        
+        choices: ["My Cousin Vinny?", "My Big Fat Greek Wedding?", "Goodfellas?", "Air America?"],
+        
+        correctAnswer: "Good Fellas",
+    },
+    
+    {
+        question:
+        "Who had the first number one Rap Single?",
+        
+        choices:["MC Hammer", "DJ Jazzy Jeff and the Fresh Prince", "Vanilla Ice?", "Public Enemy?"],
+        
+        correctAnswer: "Vanilla Ice",
+    },
 
         
-    trivia = ["Who lit the Olympics Torch       at the 1996 Summer Olympics?",
-        [choices]
-        ["The Counting Crows?", "Mayor Andrew Young?", "President Clinton", "Muhammed Ali","Chris Harwicke and Jenny McCarthy"]
-        [correctAnswer]["Muhammed Ali"]
-    ]
+    {   
+        question:
+        "Who lit the Olympics Torch at the 1996 Summer Olympics?",
 
+        choices:
+        ["The Counting Crows?", "Mayor Andrew Young?", "President Clinton", "Muhammed Ali","Chris Harwicke and Jenny McCarthy"],
 
-        Question3:["Which stunning pair of FBI Agents should my girlfriend been dating?",
-        [choices]["Agent Scully?","Agent Starling?","Agent Cooper?", "Agent Mulder"]
-        [correctAnswer]["Agent Scully"]
-        document.write(trivia)
+        correctAnswer: "Muhammed Ali",
+    },
+   
+    {   
+        question:
+        "Which stunning pair of FBI Agents should my girlfriend been dating?",
+        
+        choices:
+        ["Agent Scully?","Agent Starling?","Agent Cooper?", "Agent Mulder"],
+        
+        correctAnswer: "Agent Scully",
+    }
+]
+     var game = {
+
+      win : 0,
+      loss : 0,
+      unanswered : 0,
+      counter : 0
+ };
+ //setinterval to count down time
+ function timeLeft(){
+	$("#s_timer").countdowntimer({
+		seconds : 1000
+                ,size : "lg"
+	});
 };
 
-var game = 
-{
-    var: 'win',
-    var: 'lost',
-    var: 'unanswered',
-    var: 'counter'
+// in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+    // console log 10 seconds left
+ 
+  
+  
 
-    function setInterval() {
-        (function(){ alert("Times"); }, 5000);
-    }
-        setInterval(fiveSeconds, 1000 * 5);
-        setInterval(tenSeconds, 1000 * 10);
-        setInterval(timesUp("Time's Up!"));
-    }
-        $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-console.log("10 seconds left");
-    }
-     $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
-console.log("5 seconds left");
-    }
-    function timeUp() {
-    console.log("done");
-    $("#time-left").append("<h2>Time's Up!</h2>");
-console.log("time is up");
-  },
- //setinterval to countdown time
-  function setInterval(UpdateDisplay,1);
-    function updateDisplay(){
-        var value = parsInt($('#timer').find('.value').text(),10);
-        value++;
-        $('#timer').find('.value').text(value);
-    }, 
-    (function setTimeout(tenSeconds, 1000):;
 //for loop to check answers    
-    var trivia;
-    for (i = 0; i < trivia.length; i++){
-    text += trivia[i] + "<questions>";                                           
-}
-//IF statement to check if game is done
-if (questions){
-    $("#You Win!".click(){
+//var trivia;
+  //   for (i = 0; i < trivia.length; i++){
+    // text += trivia[i] + "<questions>";                                           
+ //}
+
+
+
+// //IF statement to check if game is done
+// if (questions){
+//     $("#You Win!".click(){
         
-    };
-} else
-(#"Sorry Try Again"();
-});
+//     };
+// } else
+// ("Sorry Try Again"();
+// });
 
-else/if (condition) {
-    ("Try again!") 
-}
-//check when all questions are done
+// else if (condition) {
+//     ("Try again!") 
+// }
+// //check when all questions are done
 
-else/if
+// else/if
 
-//clear area
-//display the Trivia.win Trivia.lost Trivia.unanaswered
+// //clear area
+// //display the Trivia.win Trivia.lost Trivia.unanaswered
 
-//function for results
-updateDisplay {
-    "Results"
-}
-//make sure user clicks correct answer
-//if userChoices.val()===""
-//unaswer ++
-//if (userChoices)!=answer)
-//lost++
+// //function for results
+// updateDisplay {
+//     "Results"
+// }
+// //make sure user clicks correct answer
+// //if userChoices.val()===""
+// //unaswer ++
+// //if (userChoices)!=answer)
+// //lost++
